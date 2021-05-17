befd990445fc233c4839e38ed0af0f0c8d8a97e9
jacopoColozzi <81474943+jacopoColozzi@users.noreply.github.com>
2021-05-17 23:45:28 +0200

    un altro pò di stile

--- connection.php
+++ connection.php
@@ -1,16 +1,18 @@
 <?php
-    $nomeDB = "database_esposizione";
+    $nomeDB = "database";
     $tabellaProdotti = "prodotti";
     $tabellaUtenti = "utenti";
     $tabellaFrutta = "frutta";
     $tabellaFiori = "fiori";
-    
+    $utente = "root";
+    $password = "";
+
     if ($db_da_creare == true) { // Questa variebile va settata prima di "require_once("connection.php");". 
                                 //Di default è falso e verrà quindi non creato il db
         echo "Creo il database...<br />";
-        $connessione = new mysqli("localhost","root", "");
+        $connessione = new mysqli("localhost", $utente , $password);
     } else {
-        $connessione = new mysqli("localhost","root", "", $nomeDB);
+        $connessione = new mysqli("localhost", $utente , $password, $nomeDB);
     }
     if (mysqli_errno($connessione)) {
         printf("Oops, abbiamo problemi con la connessione al db: %s\n", mysqli_error($connessione));
